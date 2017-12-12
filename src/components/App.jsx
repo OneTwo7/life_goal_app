a163b5c7021ab46464ea6c896e69e96915f6de29
onetwo7 <lahoohal@gmail.com>
2017-12-12 15:09:14 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const App = () => (
+  <div className="container">
+    <div className="row">
+      <div className="col-md-6 offset-md-3">
+        <h1>App Component</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit
+          amet lorem vitae augue porttitor placerat. Proin nibh orci, volutpat
+          quis nunc nec, pretium sagittis elit. Nullam auctor massa a lobortis
+          porta. Fusce et diam in ante dictum iaculis. Fusce quis odio quis
+          nunc imperdiet aliquam. Sed vel convallis turpis. Phasellus porttitor
+          nibh velit, non condimentum mauris fermentum et. Aliquam in orci
+          tristique, elementum diam ac, sagittis massa. Maecenas vehicula
+          porttitor est, eget bibendum sem tincidunt venenatis.
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
